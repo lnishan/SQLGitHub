67e679ebbc954ba5a3df0b9ac55b09f6c5f21525
Jasmine Chen <j.lin013@gmail.com>
2017-09-28 20:41:05 +0800

    Move Sample Usage of SgTable from SQLGitHub.py to table.py

--- SQLGitHub.py
+++ SQLGitHub.py
@@ -1,5 +1,4 @@
 from components import core
-from components import table
 
 try:
     import config
@@ -10,16 +9,6 @@ else:
 
 
 if __name__ == "__main__":
-    table = table.SgTable()
-    table.Append([1, 2, 3])
-    table.Append([2, 4, 6])
-    table.Append([3, 6, 9])
-    for row in table:
-        print(row)
-    print(table[1])
-    table[1] = [2, 2, 2]
-    print(table[1])
-    exit()
     token = token or raw_input("Please enter your GitHub token (which can be obtained from https://github.com/settings/tokens):")
     sqlserv = core.SQLGitHub(token)
     org = sqlserv._github.get_organization("google")
--- components/table.py
+++ components/table.py
@@ -1,4 +1,16 @@
-"""A class to store tables."""
+"""A class to store tables.
+
+Sample Usage:
+    table = table.SgTable()
+    table.Append([1, 2, 3])
+    table.Append([2, 4, 6])
+    table.Append([3, 6, 9])
+    for row in table:
+        print(row)
+    print(table[1])
+    table[1] = [2, 2, 2]
+    print(table[1])
+"""
 
 
 class SgTable:
