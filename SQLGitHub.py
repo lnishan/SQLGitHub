dd51b0f9465272ffa76b7b480599190c013a0940
Jasmine Chen <j.lin013@gmail.com>
2017-09-29 15:10:17 +0800

    Finish integration. SQLGitHub now features a simple prompt to accept user queries. (need bugfix on utf-8 encoding)

--- SQLGitHub.py
+++ SQLGitHub.py
@@ -11,6 +11,4 @@ else:
 if __name__ == "__main__":
     token = token or raw_input("Please enter your GitHub token (which can be obtained from https://github.com/settings/tokens):")
     sqlserv = top_level.SQLGitHub(token)
-    org = sqlserv._github.get_organization("google")
-    for repo in org.get_repos():
-        print(repo.name)
+    sqlserv.Start()
--- components/top_level.py
+++ components/top_level.py
@@ -1,8 +1,33 @@
+import sys
+
 from github import Github
+import tokenizer
+import parser
 
 
 class SQLGitHub:
     """Meta Component for SQLGitHub."""
 
+    _PROMPT_STR = "SQLGitHub> "
+
     def __init__(self, token):
         self._github = Github(token)
+        self._parser = parser.SgParser(self._github)
+
+    def Start(self):
+        while True:
+            sys.stdout.write(self._PROMPT_STR)
+            sys.stdout.flush()
+            sql = sys.stdin.readline().strip()
+            if sql in ["q", "exit"]:
+                break
+            tokens = tokenizer.SgTokenizer.Tokenize(sql)
+            try:
+                session = self._parser.Parse(tokens)
+            except NotImplementedError:
+                sys.stderr.write("Not implemented command tokens in SQL.\n")
+            except SyntaxError:
+                sys.stderr.write("SQL syntax incorrect.\n")
+            else:
+                result = session.Execute()
+                print(result)
